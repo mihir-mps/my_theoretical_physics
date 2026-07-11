083437723770b3f36558f7e2e89a1c7eeeb5ad5b
Mihir Pratap <pratapmihir08@gmail.com>
2026-07-11 08:59:26 +0530

    Add files via upload

---
+++ quantum_entangement.py
@@ -0,0 +1,22 @@
+import bluequbit
+from qiskit import QuantumCircuit
+import matplotlib.pyplot as plt
+
+bq = bluequbit.init("V0ADIqu8zcW0hjebFL1oPJZMr9s2OuMY")
+
+
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.cx(0, 1)
+qc.measure_all()
+
+print("Text Circuit Diagram:")
+print(qc.draw(output='text'))
+
+qc.draw(output='mpl')
+plt.show()
+
+
+
+result = bq.run(qc, device='quantum', shots=100)
+print("\nEntanglement Results:", result.get_counts()) 
